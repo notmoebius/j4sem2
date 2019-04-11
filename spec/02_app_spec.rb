a792dcb290c69461115ee9faadece952b375512e
notmoebius <notmoebius@gmail.com>
2019-04-11 18:29:56 +0200

    fix spec 02

--- spec/02_app_spec.rb
+++ spec/02_app_spec.rb
@@ -2,7 +2,7 @@ require_relative "../lib/02_app"
 
 describe "scrap get_townhall should return something" do
     it "should return something lol" do
-        expect(get_townhall_email(townhall_URL)).not_to be_nil  # mettre url mairie
+        expect(get_townhall_email("http://annuaire-des-mairies.com/95/ambleville.html")).not_to be_nil  
         expect(get_townhall_urls).not_to be_nil
     end
 end 
