eb93be5c88bfd17776077c5d75838a9fdb9e970a
notmoebius <notmoebius@gmail.com>
2019-04-11 18:36:58 +0200

    fix spec exo 0

--- spec/00_app_spec.rb
+++ spec/00_app_spec.rb
@@ -2,10 +2,10 @@ require_relative  "../lib/00_app"
 
 describe "crypto_scraper return array of hash" do
     it "should return array, and array is not nil" do
-      expect(crypto_scraper).not_to be_nil
+      expect(crypto_scrapper).not_to be_nil
     end
     it "should return a hash" do 
-      expect(crypto_scraper).to be_truthy
+      expect(crypto_scrapper).to be_truthy
     end
 end
 
